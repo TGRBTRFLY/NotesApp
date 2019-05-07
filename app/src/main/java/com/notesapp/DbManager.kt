e18cad7d8f1668f568a83f8e21e097b86a0e92d6
PagePetty <pagevpetty@live.com>
2019-05-07 13:16:00 -0600

    115.0 list notes from Sqlite database

--- app/src/main/java/com/notesapp/DbManager.kt
+++ app/src/main/java/com/notesapp/DbManager.kt
@@ -2,8 +2,10 @@ package com.notesapp
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.database.sqlite.SQLiteQueryBuilder
 import android.widget.Toast
 
 class DbManager {
@@ -16,9 +18,9 @@ class DbManager {
     val dbVersion = 1
     // CREATE TABLE IF NOT EXISTS (ID INTEGER PRIMARY KEY, title TEXT, Description TEXT);"
     val sqlCreatedTable =
-        "CREATE TABLE IF NOT EXISTS " +
-            dbTable + " (" + callID + " INTEGER PRIMARY KEY, " + callTitle +
-            " TEXT, " + callDes + " TEXT);"
+            "CREATE TABLE IF NOT EXISTS " +
+                    dbTable + " (" + callID + " INTEGER PRIMARY KEY, " + callTitle +
+                    " TEXT, " + callDes + " TEXT);"
     var sqlDB: SQLiteDatabase? = null
 
     constructor(context: Context) {
@@ -42,12 +44,29 @@ class DbManager {
         override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
             db!!.execSQL("Drop table IF EXISTS " + dbTable)
         }
-
     }
 
     fun Insert(values: ContentValues): Long {
         val ID = sqlDB!!.insert(dbTable, "", values)
         return ID
+    }
 
+    fun Query(projection: Array<String>, selection: String, selectionArgs: Array<String>, sortOrder: String): Cursor {
+
+        val qb = SQLiteQueryBuilder()
+        qb.tables = dbTable
+        val cursor = qb.query(sqlDB, projection, selection, selectionArgs, null, null, sortOrder)
+        return cursor
     }
 }
+
+
+
+
+
+
+
+
+
+
+
